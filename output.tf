f869f0207e99359c61bf773af445096d83c6feb9
Behzad-Rajabalipour <behzad.rajabalipour@gmail.com>
2024-10-29 17:55:04 -0400

    change output

--- output.tf
+++ output.tf
@@ -17,6 +17,10 @@ output "vpc_id" {
   value = aws_vpc.main.id
 }
 
+output "vpc_cidr" {
+  value = aws_vpc.main.cidr_block
+}
+
 output "public_route_table" {
   value = aws_route_table.public_route_table
 }
